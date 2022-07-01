a978390674283975176882752c357aedb2c50342
maximfd <m.fedenyov@gmail.com>
2022-07-01 16:25:19 +0300

    Update API call to fetch forecast data

--- script.js
+++ script.js
@@ -15,23 +15,74 @@ const searchInput = document.querySelector('.search-bar')
 const dateTime = document.querySelector('.date')
 const errorMsg = document.querySelector('.error-message')
 
-function getWeather(location) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${key}&units=metric`,
+function getCoords(location) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${key}`,
         { mode: 'cors' })
         .then(response => response.json())
-        .then(data => {
-            const weatherObj = processData(data)
-            displayData(weatherObj)
+        .then(location => {
+            displayLocation(location)
+            getWeather(location[0].lat, location[0].lon)
         })
         .catch(error => {
             errorMsg.style.visibility = 'visible'
         })
 }
 
-function displayData(weatherObj) {
-    errorMsg.style.visibility = 'hidden'
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${key}&units=metric`,
+        { mode: 'cors' })
+        .then(response => response.json())
+        .then(weather => {
+            const currentWeather = processCurrentWeather(weather.current)
+            displayCurrentWeather(currentWeather)
+            const forecast = processForecastWeather(weather.daily)
+            console.log(forecast);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
 
-    locationTitle.innerHTML = `${weatherObj.city}, <span class="country">${weatherObj.country}</span>`
+function processCurrentWeather(data) {
+    return {
+        currentTemp: Math.round(data.temp),
+        feelsTemp: Math.round(data.feels_like),
+        hum: data.humidity,
+        pressure: data.grnd_level ? data.grnd_level : data.pressure,
+        wind: data.wind_speed,
+        tag: data.weather[0].description,
+        icon: data.weather[0].icon,
+    }
+}
+
+function processForecastWeather(arr) {
+    return arr.map(element => {
+        return {
+            date: new Date(element.dt * 1000),
+            temp: element.temp.day,
+            hum: element.humidity,
+            pressure: element.pressure,
+            icon: element.weather[0].icon,
+            tag: element.weather[0].description
+        }
+    });
+}
+
+searchForm.addEventListener('submit', searchUserWeather)
+
+function searchUserWeather(e) {
+    e.preventDefault()
+    getCoords(searchInput.value)
+    searchInput.value = ''
+    searchInput.blur()
+}
+
+function displayLocation(data) {
+    locationTitle.innerHTML = `${data[0].name}, <span class="country">${data[0].country}</span>`
+}
+
+function displayCurrentWeather(weatherObj) {
+    errorMsg.style.visibility = 'hidden'
 
     mainTempValue.textContent = weatherObj.currentTemp
     feelsTempValue.textContent = weatherObj.feelsTemp
@@ -43,32 +94,6 @@ function displayData(weatherObj) {
     pressureValue.innerHTML = `${weatherObj.pressure}<span class="feature__unit">hPa</span>`
     windValue.innerHTML = `${weatherObj.wind}<span class="feature__unit">m/s</span>`
     humidityValue.innerHTML = `${weatherObj.hum}<span class="feature__unit">%</span>`
-}
-
-function processData(data) {
-    const currentTemp = Math.floor(data.main.temp)
-    const feelsTemp = Math.ceil(data.main.feels_like)
-
-    return {
-        city: data.name,
-        country: data.sys.country,
-        currentTemp: currentTemp,
-        feelsTemp: feelsTemp,
-        hum: data.main.humidity,
-        pressure: data.main.grnd_level ? data.main.grnd_level : data.main.pressure,
-        wind: data.wind.speed,
-        tag: data.weather[0].description,
-        icon: data.weather[0].icon,
-    }
-}
-
-searchForm.addEventListener('submit', searchUserWeather)
-
-function searchUserWeather(e) {
-    e.preventDefault()
-    getWeather(searchInput.value)
-    searchInput.value = ''
-    searchInput.blur()
 }
 
 function renderDateTime() {
@@ -86,4 +111,4 @@ function renderDateTime() {
 }
 
 // getWeather('Amsterdam')
-renderDateTime()
+// renderDateTime()
